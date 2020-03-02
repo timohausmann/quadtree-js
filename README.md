f85c65b2e02cf88f8aa236f19f2a04557884a21d
Timo Hausmann <hello.timohausmann@gmail.com>
2020-03-02 17:02:10 +0100

    1.2.0: store objects on leaf nodes only

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # quadtree-js
 
-This is a JavaScript Quadtree implementation of the Java Methods described on [gamedevelopment.tutsplus.com by Steven Lambert](https://gamedevelopment.tutsplus.com/tutorials/quick-tip-use-quadtrees-to-detect-likely-collisions-in-2d-space--gamedev-374):
+This is a JavaScript Quadtree implementation based on the Java Methods described on [gamedevelopment.tutsplus.com by Steven Lambert](https://gamedevelopment.tutsplus.com/tutorials/quick-tip-use-quadtrees-to-detect-likely-collisions-in-2d-space--gamedev-374):
 
 > Many games require the use of collision detection algorithms to determine when two objects have collided, but these algorithms are often expensive operations and can greatly slow down a game. One way to speed things up is to reduce the number of checks that have to be made. Two objects that are at opposite ends of the screen can not possibly collide, so there is no need to check for a collision between them. This is where a quadtree comes into play.
 
-Please read the tutorial for a better understanding.
+This implementation can store and retrieve rectangles in a recursive 2D Quadtree. Every Quadtree node can hold a maximum of number objects before it splits into four subnodes. Objects are only stored on leaf nodes (the lowest level). If an object overlaps into multiple leaf nodes, a reference to the object is stored in each node. 
 
 ## Demos
 
 * [Simple Demo](http://timohausmann.de/quadtree.js/simple.html) – add static objects and see the Quadtree split
 * [Dynamic Demo](http://timohausmann.de/quadtree.js/dynamic.html) – continuously track moving objects
+* [Benchmark v1.2](http://timohausmann.de/quadtree.js/test-10000-1.2.0.html) - Performance test with 10.000 objects
+* [Benchmark v1.1.3](http://timohausmann.de/quadtree.js/test-10000-1.1.3.html) - Performance test with 10.000 objects (old implementation)
 
 ## Install
 
@@ -45,14 +47,15 @@ var myTree = new Quadtree({
 
 > MAX_OBJECTS defines how many objects a node can hold before it splits and MAX_LEVELS defines the deepest level subnode.
 
-If you want to specify max_objects and max_levels on your own, you can pass them as a 2nd and 3rd argument
+If you want to specify `max_objects` and `max_levels` on your own, you can pass them as a 2nd and 3rd argument. Increasing `max_levels` may have an impact on performance depending on your objects. Using a low value for `max_levels` increases the Quadtree performance but will most likely return more candidates.
+
 <pre>
 var myTree = new Quadtree({
 	x: 0,
 	y: 0,
 	width: 800,
 	height: 600
-}, 5, 8);
+}, 15, 6);
 </pre> 
 
 Insert an element in the Quadtree
@@ -65,7 +68,7 @@ myTree.insert({
 });
 </pre>
 
-Retrieve elements that "collide" with the given bounds
+Retrieve elements from nodes that intersect with the given bounds
 <pre>
 var elements = myTree.retrieve({
 	x: 150,
@@ -82,5 +85,20 @@ myTree.clear();
 
 Check out the examples for more information.
 
-There is an alternative [quadtree-js hitman branch](https://github.com/timohausmann/quadtree-js/tree/hitman) available that allows you to update and remove single objects.
-This can be handy when most of the objects in your Quadtree are static.
+## Browser Support
+
+This library is supported in all modern browsers beginning from IE9 and above. 
+
+## Changelog
+
+### 1.2.0
+
+This implementation now stores objects exclusively on leaf nodes and thus differs from the tutorial it's based on. Objects, that overlap into multiple subnodes are now referenced in each matching subnode instead of their parent node. This drastically reduces the collision candidates. Prior to 1.2.0, overlapping objects were stored in parent nodes. 
+
+### 1.1.3
+
+Support for npm and `module.exports`
+
+## Update single objects
+
+There is a (currently deprecated) [quadtree-js hitman branch](https://github.com/timohausmann/quadtree-js/tree/hitman) available that allows you to update and remove single objects. This may be handy when most of the objects in your Quadtree are static. Please raise an issue if you want to see this feature maintained in future releases.
--- quadtree.js
+++ quadtree.js
@@ -1,6 +1,6 @@
 /*
  * Javascript Quadtree 
- * @version 1.1.2
+ * @version 1.2.0
  * @licence MIT
  * @author Timo Hausmann
  * https://github.com/timohausmann/quadtree-js/
@@ -33,10 +33,10 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  	
 	 /*
 	  * Quadtree Constructor
-	  * @param Object bounds		bounds of the node, object with x, y, width, height
+	  * @param Object bounds			bounds of the node, object with x, y, width, height
 	  * @param Integer max_objects		(optional) max objects a node can hold before splitting into 4 subnodes (default: 10)
 	  * @param Integer max_levels		(optional) total max levels inside root Quadtree (default: 4) 
-	  * @param Integer level		(optional) deepth level, required for subnodes  
+	  * @param Integer level			(optional) deepth level, required for subnodes (default: 0)
 	  */
 	function Quadtree( bounds, max_objects, max_levels, level ) {
 		
@@ -44,9 +44,9 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 		this.max_levels		= max_levels || 4;
 		
 		this.level 		= level || 0;
-		this.bounds 		= bounds;
+		this.bounds 	= bounds;
 		
-		this.objects 		= [];
+		this.objects 	= [];
 		this.nodes 		= [];
 	};
 	
@@ -56,7 +56,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 	 */
 	Quadtree.prototype.split = function() {
 		
-		var 	nextLevel	= this.level + 1,
+		var nextLevel	= this.level + 1,
 			subWidth	= Math.round( this.bounds.width / 2 ),
 			subHeight 	= Math.round( this.bounds.height / 2 ),
 			x 		= Math.round( this.bounds.x ),
@@ -99,38 +99,45 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 	/*
 	 * Determine which node the object belongs to
 	 * @param Object pRect		bounds of the area to be checked, with x, y, width, height
-	 * @return Integer		index of the subnode (0-3), or -1 if pRect cannot completely fit within a subnode and is part of the parent node
+	 * @return Array			an array of indexes of the subnodes (0-3) pRect is touching
 	 */
 	Quadtree.prototype.getIndex = function( pRect ) {
 		
-		var 	index 			= -1,
+		var indexes = [],
 			verticalMidpoint 	= this.bounds.x + (this.bounds.width / 2),
-			horizontalMidpoint 	= this.bounds.y + (this.bounds.height / 2),
-	 
-			//pRect can completely fit within the top quadrants
-			topQuadrant = (pRect.y < horizontalMidpoint && pRect.y + pRect.height < horizontalMidpoint),
-			
-			//pRect can completely fit within the bottom quadrants
-			bottomQuadrant = (pRect.y > horizontalMidpoint);
-		 
-		//pRect can completely fit within the left quadrants
-		if( pRect.x < verticalMidpoint && pRect.x + pRect.width < verticalMidpoint ) {
-			if( topQuadrant ) {
-				index = 1;
-			} else if( bottomQuadrant ) {
-				index = 2;
-			}
-			
-		//pRect can completely fit within the right quadrants	
-		} else if( pRect.x > verticalMidpoint ) {
-			if( topQuadrant ) {
-				index = 0;
-			} else if( bottomQuadrant ) {
-				index = 3;
-			}
+			horizontalMidpoint 	= this.bounds.y + (this.bounds.height / 2);	
+
+		//top left quad
+		if(pRect.x < verticalMidpoint && pRect.y < horizontalMidpoint) { //top-left corner
+			indexes.push(1);
+		}		
+
+		//top right quad
+		if(
+			(pRect.x > verticalMidpoint && pRect.y < horizontalMidpoint) || //top-left corner
+			(pRect.x + pRect.width > verticalMidpoint && pRect.y < horizontalMidpoint) //top-right corner
+			) {
+			indexes.push(0);
+		}
+
+		//bottom left quad
+		if(
+			pRect.x < verticalMidpoint && pRect.y > horizontalMidpoint || //top-left corner
+			(pRect.x < verticalMidpoint && pRect.y + pRect.height > horizontalMidpoint) //bottom-left corner
+			) {
+			indexes.push(2);
+		}
+
+		//bottom right quad
+		if(
+			(pRect.x > verticalMidpoint && pRect.y > horizontalMidpoint) || //top-left corner
+			((pRect.x + pRect.width > verticalMidpoint) && pRect.y > horizontalMidpoint) || //top-right corner
+			((pRect.x + pRect.width > verticalMidpoint) && pRect.y + pRect.height > horizontalMidpoint) //bottom right corner
+			) {
+			indexes.push(3);
 		}
 	 
-		return index;
+		return indexes;
 	};
 	
 	
@@ -142,39 +149,40 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 	 */
 	Quadtree.prototype.insert = function( pRect ) {
 		
-		var 	i = 0,
-	 		index;
+		var i = 0,
+	 		indexes;
 	 	
-	 	//if we have subnodes ...
-		if( typeof this.nodes[0] !== 'undefined' ) {
-			index = this.getIndex( pRect );
+	 	//if we have subnodes, call insert on matching subnodes
+		if(this.nodes.length) {
+			indexes = this.getIndex( pRect );
 	 
-		  	if( index !== -1 ) {
-				this.nodes[index].insert( pRect );	 
-			 	return;
+			for(i=0; i<indexes.length;i++) {
+				this.nodes[indexes[i]].insert( pRect );	 
 			}
+			return;
 		}
 	 
+		//otherwise, store object here
 	 	this.objects.push( pRect );
-		
+
+		//max_objects reached
 		if( this.objects.length > this.max_objects && this.level < this.max_levels ) {
-			
+
 			//split if we don't already have subnodes
-			if( typeof this.nodes[0] === 'undefined' ) {
+			if(!this.nodes.length) {
 				this.split();
 			}
 			
-			//add all objects to there corresponding subnodes
-			while( i < this.objects.length ) {
-				
-				index = this.getIndex( this.objects[ i ] );
-				
-				if( index !== -1 ) {					
-					this.nodes[index].insert( this.objects.splice(i, 1)[0] );
-				} else {
-					i = i + 1;
-			 	}
-		 	}
+			//add all objects to their corresponding subnode
+			for(i=0; i<this.objects.length; i++) {
+				indexes = this.getIndex( this.objects[ i ] );
+				for(var k=0; k<indexes.length;k++) {
+					this.nodes[indexes[k]].insert( this.objects[i] );
+				}
+			}
+
+			//clean up this node
+			this.objects = [];
 		}
 	 };
 	 
@@ -182,27 +190,24 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 	/*
 	 * Return all objects that could collide with the given object
 	 * @param Object pRect		bounds of the object to be checked, with x, y, width, height
-	 * @Return Array		array with all detected objects
+	 * @Return Array			array with all detected objects
 	 */
 	Quadtree.prototype.retrieve = function( pRect ) {
 	 	
-		var 	index = this.getIndex( pRect ),
+		var indexes = this.getIndex( pRect ),
 			returnObjects = this.objects;
 			
-		//if we have subnodes ...
-		if( typeof this.nodes[0] !== 'undefined' ) {
-			
-			//if pRect fits into a subnode ..
-			if( index !== -1 ) {
-				returnObjects = returnObjects.concat( this.nodes[index].retrieve( pRect ) );
-				
-			//if pRect does not fit into a subnode, check it against all subnodes
-			} else {
-				for( var i=0; i < this.nodes.length; i=i+1 ) {
-					returnObjects = returnObjects.concat( this.nodes[i].retrieve( pRect ) );
-				}
+		//if we have subnodes, retrieve their objects
+		if(this.nodes.length) {
+			for(var i=0; i<indexes.length;i++) {
+				returnObjects = returnObjects.concat( this.nodes[indexes[i]].retrieve( pRect ) );
 			}
 		}
+
+		//remove duplicates
+		returnObjects = returnObjects.filter(function(item, index) {
+			return returnObjects.indexOf(item) >= index;
+		});
 	 
 		return returnObjects;
 	};
@@ -216,7 +221,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 		this.objects = [];
 	 
 		for( var i=0; i < this.nodes.length; i=i+1 ) {
-			if( typeof this.nodes[i] !== 'undefined' ) {
+			if(this.nodes.length) {
 				this.nodes[i].clear();
 		  	}
 		}
